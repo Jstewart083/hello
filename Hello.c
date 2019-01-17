beb38d42f55e62e7f650c26987cfc40689c47b26
Jstewart083 <Jstewart083@hotmail.com>
2019-01-17 11:09:16 -0600

    Initial commit

---
+++ Hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Jacob Stewart, Brent Miller
+ * Date: 2019/01/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+	
+int main(int argc, char **argv) {
+  printf("Jacob Stewart, Brent Miller");
+
+  return 0;
+}
